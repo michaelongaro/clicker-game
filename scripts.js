e559c5a50fb70026d02894e10dc122058d08ced5
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2021-10-27 17:03:53 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,80 @@
+window.onload = ()=> {
+
+    let square = document.getElementById("square")
+
+    let startTime, endTime, timeDiff;
+    let initTimer = true;
+
+    square.addEventListener("click", ()=>{
+
+        timeDiff = end() //ends timer and saves value to timeDiff
+        start()  //restarts timer
+
+        let far_enough_away = false
+
+        let current_x = square.getBoundingClientRect().left + 50;
+        let current_y = square.getBoundingClientRect().top + 50;
+
+        let bounded_randx;
+        let bounded_randy;
+
+       // while (!far_enough_away) {
+            bounded_randx = Math.floor(Math.random() * (window.screen.width-100)) + 100;
+            bounded_randy = Math.floor(Math.random() * (window.screen.height-100)) + 100;
+
+            // if (bounded_randx < current_x) {
+            //     bounded_randx *= -1
+            // }
+
+            // if (bounded_randy < current_y) {
+            //     bounded_randy *= -1
+            // }
+
+            // if ((bounded_randx > 100 && bounded_randx < (window.screen.width - 100))
+            //     && (bounded_randy > 100 && bounded_randy < (window.screen.height - 100))) {
+            
+            //         far_enough_away = true
+            // }
+
+            
+       // }
+
+        // console.log(bounded_randx);
+        // console.log(bounded_randy);
+        console.log(timeDiff);
+
+        anime({
+            targets: '#square',
+            translateX: bounded_randx,
+            translateY: bounded_randy,
+            rotate: '1turn',
+            backgroundColor: randomColor,
+            duration: timeDiff * .85
+        });
+
+
+
+    })
+
+    function randomColor() {
+        let r = Math.floor(Math.random() * 256)
+        let g = Math.floor(Math.random() * 256)
+        let b = Math.floor(Math.random() * 256)
+        return `rgb(${r},${g},${b})`
+    }
+
+    function start() {
+        startTime = performance.now();
+    }
+
+    function end() {
+        if (initTimer) {
+            initTimer = false;
+            return
+        }
+
+        endTime = performance.now();
+        return endTime - startTime;
+    }
+
+}
