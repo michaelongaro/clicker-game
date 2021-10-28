d1cf5b809c29b8e27b2cda0197ea99a61fec673f
Michael Ongaro <35980684+michaelongaro@users.noreply.github.com>
2021-10-28 16:29:14 -0500

    added menu screen, game timer, and now respects browser size

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
 window.onload = ()=> {
 
-    let square = document.getElementById("square")
+    let startScreen = document.getElementById("start-screen");
+    let startSquare = document.getElementById("start-box");
+    let square = document.getElementById("square");
+    let counter = document.getElementById("counter");
+    let score = document.getElementById("score");
 
     let startTime, endTime, timeDiff;
     let initTimer = true;
+
+    let timer;
+
+    let counter_clicks = 0;
+
+    startSquare.addEventListener("click", ()=>{
+        start30Seconds();
+
+        counter_clicks = 0;
+        counter.innerHTML = counter_clicks;
+        newSquareLocation(Math.floor(Math.random() * (window.screen.width - 350)) + 100,
+                            Math.floor(Math.random() * (window.screen.height - 350)) + 100,
+                            1)
+        
+        displayGameScreen();
+    });
 
     square.addEventListener("click", ()=>{
         if (initTimer) {
@@ -23,47 +43,18 @@ window.onload = ()=> {
         let bounded_randx;
         let bounded_randy;
 
-       // while (!far_enough_away) {
-            bounded_randx = Math.floor(Math.random() * (window.screen.width-100)) + 100;
-            bounded_randy = Math.floor(Math.random() * (window.screen.height-100)) + 100;
-
-            // if (bounded_randx < current_x) {
-            //     bounded_randx *= -1
-            // }
-
-            // if (bounded_randy < current_y) {
-            //     bounded_randy *= -1
-            // }
-
-            // if ((bounded_randx > 100 && bounded_randx < (window.screen.width - 100))
-            //     && (bounded_randy > 100 && bounded_randy < (window.screen.height - 100))) {
-            
-            //         far_enough_away = true
-            // }
-
-            
-       // }
-
-        // console.log(bounded_randx);
-        // console.log(bounded_randy);
-        console.log(timeDiff);
+        bounded_randx = Math.floor(Math.random() * (window.screen.width-350)) + 100;
+        bounded_randy = Math.floor(Math.random() * (window.screen.height-350)) + 100;
 
         if (timeDiff > 5000) {
             timeDiff = 5750
         }
 
-        anime({
-            targets: '#square',
-            translateX: bounded_randx,
-            translateY: bounded_randy,
-            rotate: '1turn',
-            backgroundColor: randomColor,
-            duration: timeDiff * .85
-        });
+        counter_clicks += 1;
+        counter.innerHTML = counter_clicks;
 
-
-
-    })
+        newSquareLocation(bounded_randx, bounded_randy, timeDiff);
+    });
 
     function randomColor() {
         let r = Math.floor(Math.random() * 256)
@@ -79,6 +70,48 @@ window.onload = ()=> {
     function end() {
         endTime = performance.now();
         return endTime - startTime;
+    }
+
+    function displayGameScreen() {
+        startScreen.style.display = "none";
+        startSquare.style.display = "none";
+        score.style.display = "none";
+        square.style.display = "block";
+        counter.style.display = "block";
+    }
+
+    function reset_screen() {
+        square.style.display = "none";
+        counter.style.display = "none";
+        startScreen.style.display = "flex";
+        startSquare.style.display = "block";
+        score.style.display = "block";
+
+        if (counter_clicks != 0) {
+            add_previous_score();
+        }
+    }
+
+    function add_previous_score() {
+        score.innerHTML = `Previous Score: ${counter_clicks}`;
+    }
+
+    function start30Seconds() {
+        timer = setTimeout(reset_screen, 30000);
+    }
+
+    // what to do when the previous duration is very long, and then the square is clicked almost
+    // immediately? it rubberbands the square for a second and then returns to the first position
+    // as if it wasn't clicked at all.
+    function newSquareLocation(x, y, delay) {
+        anime({
+            targets: '#square',
+            translateX: x,
+            translateY: y,
+            rotate: '1turn',
+            backgroundColor: randomColor,
+            duration: delay * .85
+        });
     }
 
 }
